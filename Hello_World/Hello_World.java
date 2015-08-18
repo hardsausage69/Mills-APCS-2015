81d551bbde0e2cbb978575188d1c4993f99831aa
hardsausage69 <98allenng@gmail.com>
2015-08-18 09:20:23 -0700

    Hello there

---
+++ Hello_World/Hello_World.java
@@ -0,0 +1,15 @@
+
+/**
+ * DUDUDDUDUDUDUDU
+ * 
+ * @author Allen Ng 
+ * @version 8-18-15
+ */
+public class Hello_World
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World! From Allen Ng");
+        //Display the string
+    }        
+}
