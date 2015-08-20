bd6acdec96c27911807d23b2a78dbefdda8c273e
hardsausage69 <98allenng@gmail.com>
2015-08-19 20:37:34 -0700

    asdf

---
+++ Stewie2/Stewie2.java
@@ -0,0 +1,23 @@
+
+/**
+ * DUDUDUUDUDUDUDUDU
+ * 
+ * Allen Ng 
+ * 1 8-19-15
+ */
+public class Stewie2
+{
+   public static void main(String[] args) {
+       System.out.println("//////////////////////");
+       System.out.println("|| Victory is mine! ||");
+       System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+       stewie();
+       stewie();
+       stewie();
+       stewie();
+    }
+    public static void stewie(){
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
