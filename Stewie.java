6e355578b8e842679132eec6efd1b5637468bbe2
hardsausage69 <98allenng@gmail.com>
2015-08-19 19:56:23 -0700

    stewie stuff

---
+++ Stewie.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Stewie here.
+ * 
+ * Allen Ng
+ * 1 8-19-15
+ */
+public class Stewie
+{
+   public static void main(String[] args) {
+       System.out.println("//////////////////////");
+       System.out.println("|| Victory is mine! ||");
+       System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
