5fc4facf3b513f2c8c41e49c912b6a769392f50a
hardsausage69 <98allenng@gmail.com>
2015-08-21 08:37:51 -0700

    pikachu

---
+++ pikachu/Pikachu.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Pikachu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pikachu
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Pikachu welcomes you to the world of Pokemon!");
+        System.out.println("\t (\\__/)");
+        System.out.println("\t (0^.^)");
+        System.out.println("\tz(_\")(\")");
+    }
+}
