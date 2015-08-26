e0dafb2085b0c9fb4c6093dd12270b3e38e8430b
hardsausage69 <98allenng@gmail.com>
2015-08-25 17:07:43 -0700

    asdf

---
+++ Something/Jack.java
@@ -0,0 +1,81 @@
+
+/**
+ * Write a description of class Something here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Jack
+{
+public static void main(String[] args)
+{
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+jack();
+}
+public static void jack() 
+{
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+System.out.println("All work and no play makes Jack a dull boy.");
+}
+}
