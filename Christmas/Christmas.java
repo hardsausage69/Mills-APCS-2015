1c77ac34818a0c8e801a263b0532d47a9d3e3dba
hardsausage69 <98allenng@gmail.com>
2015-08-27 14:38:25 -0700

    christmas

---
+++ Christmas/Christmas.java
@@ -0,0 +1,118 @@
+public class Christmas
+{
+    public static void twelve()
+    {
+        System.out.println("Twelve drummers drumming,");
+        eleven();
+    }
+    public static void eleven()
+    {
+        System.out.println("Eleven pipers piping,");
+        ten();
+    }
+    public static void ten()
+    {
+        System.out.println("Ten Lords a leaping,");
+        nine();
+    }
+    public static void nine()
+    {
+        System.out.println("Nine ladies dancing,");
+        eight();
+    }
+    public static void eight()
+    {
+        System.out.println("Eight maids a milking,");
+        seven();
+    }
+    public static void seven()
+    {
+        System.out.println("Seven swans a swimming,");
+        six();
+    }
+    public static void six()
+    {
+        System.out.println("Six geese a laying,");
+        five();
+    }
+    public static void five()
+    {
+        System.out.println("Five golden rings,");
+        four();
+    }
+    public static void four()
+    {
+        System.out.println("Four calling birds,");
+        three();
+    }
+    public static void three()
+    {
+        System.out.println("Three french hens,");
+        two();
+    }
+    public static void two()
+    {
+        System.out.println("Two turtle doves, and");
+        one();
+    }
+    public static void one()
+    {
+        System.out.println("A partridge in a pear tree.");
+    }
+    public static void truelove()
+    {
+        System.out.println("my true love sent to me");
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("On the first day of Christmas,");
+        truelove();
+        one();
+        System.out.println();
+        System.out.println("On the second day of Christmas,");
+        truelove();
+        two();
+        System.out.println();
+        System.out.println("On the third day of Christmas,");
+        truelove();
+        three();
+        System.out.println();
+        System.out.println("On the fourth day of Christmas,");
+        truelove();
+        four();
+        System.out.println();
+        System.out.println("On the fifth day of Christmas,");
+        truelove();
+        five();
+        System.out.println();
+        System.out.println();
+        System.out.println("On the sixth day of Christmas,");
+        truelove();
+        six();
+        System.out.println();
+        System.out.println("On the seventh day of Christmas,");
+        truelove();
+        seven();
+        System.out.println();
+        System.out.println("On the eighth day of Christmas,");
+        truelove();
+        eight();
+        System.out.println();
+        System.out.println("On the ninth day of Christmas,");
+        truelove();
+        nine();
+        System.out.println();
+        System.out.println("On the tenth day of Christmas,");
+        truelove();
+        ten();
+        System.out.println();
+        System.out.println("On the eleventh day of Christmas,");
+        truelove();
+        eleven();
+        System.out.println();
+        System.out.println("On the twelfth day of Christmas,");
+        truelove();
+        twelve();
+    }
+}
+    
