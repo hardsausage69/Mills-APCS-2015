e9137fb78028ba6249761cc4df3f08e71fb2d03b
hardsausage69 <98allenng@gmail.com>
2015-09-08 09:21:27 -0700

    celsius

---
+++ Celsius/Celsius.java
@@ -0,0 +1,14 @@
+/*Allen Ng
+ * 9-8-15
+ * Celsius Attempt 1
+ */
+public class Celsius
+{
+    public static void main(String[] args)
+    {
+        double c = 100.0;
+        double f;
+        f = 9 * c / 5 + 32;
+        System.out.println(f);
+    }
+}
