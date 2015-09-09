9167b3ecabd79e0615e2b2964dc8c5b439114cfa
hardsausage69 <98allenng@gmail.com>
2015-09-08 19:47:34 -0700

    temp stuff today

---
+++ Fahrenheit/Fahrenheit.java
@@ -0,0 +1,13 @@
+/*Allen Ng
+ * 9-8-15
+ * Fahrenheit Attempt 1
+ */
+public class Fahrenheit
+{
+    public static void main(String[] args)
+    { 
+        double f = 212.0;
+        double c = 5.0 / 9.0 * ( f - 32.0 );
+        System.out.println(c);
+    }
+}
