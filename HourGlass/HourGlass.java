6b666dd22c5566f2bb2a96f13e31355519edb069
hardsausage69 <98allenng@gmail.com>
2015-09-18 08:53:36 -0700

    tempt 2

---
+++ HourGlass/HourGlass.java
@@ -0,0 +1,30 @@
+/*Allen Ng
+ * 9-18-15
+ * Attempt two of HourGlass
+ */
+public class HourGlass
+{
+    public static void main(String[] args)
+    { 
+        tip();
+        for (int lines = 1; lines <= 4; lines++)
+        { 
+            for (int i = 1; i <= 4; i++)
+            {
+                System.out.print(" ");
+            }
+            System.out.print("\\");
+            for (int j = 1; j <= 5 - lines; j++)
+            {
+                System.out.print("::");
+            }
+            System.out.print("/");
+            System.out.println();
+        }
+        System.out.println("     ||");
+        for (int lines = 1; lines <= 4; lines++)
+        {
+            for (int i = 1; i <= 5 - lines; i++)
+            {
+            }
+                
