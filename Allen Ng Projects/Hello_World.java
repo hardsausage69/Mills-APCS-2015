1043a4376bdb6a9a4b25ef1c9b5852c7fd2c9da7
hardsausage69 <98allenng@gmail.com>
2016-02-04 08:54:58 -0800

    asdf

---
+++ Allen Ng Projects/Hello_World.java
@@ -0,0 +1,15 @@
+
+/**
+ * A description of some stuff yaknow.
+ * 
+ * @author (Allen Ng) 
+ * @version (8-17-15)
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Allen");
+//Display the string
+    }
+}
