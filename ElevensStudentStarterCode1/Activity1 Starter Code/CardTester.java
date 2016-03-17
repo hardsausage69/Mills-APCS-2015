2647e12804a29d9d3f8fe226a838eb86b8748f51
hardsausage69 <98allenng@gmail.com>
2016-03-17 08:50:16 -0700

    fadasfdadssdfdf

--- ElevensStudentStarterCode1/Activity1 Starter Code/CardTester.java
+++ ElevensStudentStarterCode1/Activity1 Starter Code/CardTester.java
@@ -9,5 +9,17 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card hello = new Card("a", "spades", 2);
+		System.out.println(hello.suit());
+		System.out.println(hello.rank());
+		System.out.println(hello.pointValue());
+		
+		Card killEm = new Card("b", "diamond", 5);
+		System.out.println(killEm.toString());
+		
+		Card deez = new Card("b", "diamond", 5);
+		System.out.println(deez.matches(killEm));
+		System.out.println(deez.matches(hello));
+		
 	}
 }
